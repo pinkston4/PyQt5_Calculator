c5de8b1999163d32fe2b40026dc1b213504d51f9
john pinkston <pinkstonjack2@gmail.com>
2017-03-03 14:36:02 -0600

    refactored for pep guidlines

--- calc/app.py
+++ calc/app.py
@@ -1,50 +1,49 @@
-import sys
 from . import buttons
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+
 
 class CreateApp:
-	"""
-	Create app builds and positions the buttons
-	Methods:
-		__init__
-		createLayout
-	"""
-	def __init__(self):
-		self.createLayout()
+    """
+    Create app builds and positions the buttons
+    Methods:
+        __init__
+        createLayout
+    """
+    def __init__(self):
+        self.grid = None
+        self.create_layout()
 
-	def createLayout(self):
-		"""
-		The createLayout method builds the grid and calls the class button to 
-		create the buttons themselves. It then positions the buttons in the grid
-		"""
+    def create_layout(self):
+        """
+        The create_layout method builds the grid and calls the class button to
+        create the buttons themselves. It then positions the buttons in the grid
+        """
 
-		#grid is the layout of the app
-		grid = QGridLayout()
+        # grid is the layout of the app
+        grid = QGridLayout()
 
-		#results is the input/text/display box at the top of the app
-		results = QLineEdit()
+        # results is the input/text/display box at the top of the app
+        results = QLineEdit()
 
-		#the buttons themselves
-		buttonLabels = ['AC', 'DEL', '√', '/',
-					7, 8, 9, "*",
-					4, 5, 6, "-",
-					1, 2, 3, "+",
-					0, ".", "="]
-		row = 1
-		col = 0
-		grid.addWidget(results, 0, 0, 1, 4)
-		for button in buttonLabels:
-			if col > 3:
-				col = 0
-				row += 1
-			buttonObject = buttons.Button(button, results)
-			if button == 0:
-				grid.addWidget(buttonObject.b, row, col, 1, 2)
-				col+=1
-			else:
-				grid.addWidget(buttonObject.b, row, col, 1, 1)
-			col += 1
+        # the buttons themselves
+        button_labels = ['AC', 'DEL', '√', '/',
+                    7, 8, 9, "*",
+                    4, 5, 6, "-",
+                    1, 2, 3, "+",
+                    0, ".", "="]
+        row = 1
+        col = 0
+        grid.addWidget(results, 0, 0, 1, 4)
+        for button in button_labels:
+            if col > 3:
+                col = 0
+                row += 1
+            button_object = buttons.Button(button, results)
+            if button == 0:
+                grid.addWidget(button_object.b, row, col, 1, 2)
+                col += 1
+            else:
+                grid.addWidget(button_object.b, row, col, 1, 1)
+            col += 1
 
-		self.grid = grid
+        self.grid = grid
--- calc/buttons.py
+++ calc/buttons.py
@@ -1,52 +1,50 @@
-import sys
 import math
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+
 
 class Button:
-	"""
-	The Button class builds the button and handles the events
-	Arguments:
-		text = the text displayed on the button
-		results = what is or will be in the text box at the top of the app
-	Methods:
-		__init__
-		handleInput
-	"""
-	def __init__(self, text, results):
-		"""
-		self.b.clicked is the attribute that acts as an event listener
-		when QPushButton b is clicked it takes the value of that button,
-		the text argument, and passes text to handleInput
-		"""
-		self.b = QPushButton(str(text))
-		self.text = text
-		self.results = results
-		self.b.clicked.connect(lambda: self.handleInput(self.text))
+    """
+    The Button class builds the button and handles the events
+    Arguments:
+        text = the text displayed on the button
+        results = what is or will be in the text box at the top of the app
+    Methods:
+        __init__
+        handle_input
+    """
+    def __init__(self, text, results):
+        """
+        self.b.clicked is the attribute that acts as an event listener
+        when QPushButton b is clicked it takes the value of that button,
+        the text argument, and passes text to handle_input
+        """
+        self.b = QPushButton(str(text))
+        self.text = text
+        self.results = results
+        self.b.clicked.connect(lambda: self.handle_input(self.text))
 
-	def handleInput(self, v):
-		"""
-		The methhod handleInput determines the course of action when a 
-		button is pressed. It has one argument, v.
-		Arguments:
-			v = the value/meaning of the button
-		"""
-		if v == "=":
-			# "=" takes what is in the box and runs it through the eval function
-			res = eval(self.results.text())
-			self.results.setText(str(res))
-		elif v == "AC":
-			self.results.setText(None)
-		elif v == "√":
-			# the square root of a number
-			value = float(self.results.text())
-			self.results.setText(str(math.sqrt(value)))
-		elif v == "DEL":
-			# "DEL" acts as backspace and delets the last charachter in result
-			current_value = self.results.text()
-			self.results.setText(current_value[:-1])
-		else :
-			current_value = self.results.text()
-			new_value = current_value + str(v)
-			self.results.setText(new_value)
+    def handle_input(self, v):
+        """
+        The method handle_input determines the course of action when a
+        button is pressed. It has one argument, v.
+        Arguments:
+            v = the value/meaning of the button
+        """
+        if v == "=":
+            # "=" takes what is in the box and runs it through the eval function
+            res = eval(self.results.text())
+            self.results.setText(str(res))
+        elif v == "AC":
+            self.results.setText(None)
+        elif v == "√":
+            # the square root of a number
+            value = float(self.results.text())
+            self.results.setText(str(math.sqrt(value)))
+        elif v == "DEL":
+            # "DEL" acts as backspace and deletes the last character in result
+            current_value = self.results.text()
+            self.results.setText(current_value[:-1])
+        else:
+            current_value = self.results.text()
+            new_value = current_value + str(v)
+            self.results.setText(new_value)
